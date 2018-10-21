5b67243d9dadc411c4555406475e3be14390603d
Ansh_22 <500061120@stu.upes.ac.in>
2018-10-21 14:28:42 +0530

    Join-create table code verification done(ResDetails, using firebase)

--- app/src/main/java/com/example/ansh/ressplitter/ResDetails.java
+++ app/src/main/java/com/example/ansh/ressplitter/ResDetails.java
@@ -5,10 +5,18 @@ import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Random;
 
@@ -16,37 +24,72 @@ public class ResDetails extends AppCompatActivity {
 
     public Button join, create;
 
+    boolean exist= false;
     EditText codeinput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_res_details);
+        final DatabaseReference reference = FirebaseDatabase.getInstance().getReference("TableCode");
+
 //JOIN
         join = findViewById(R.id.join);
         join.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 AlertDialog.Builder alertDialog = new AlertDialog.Builder(ResDetails.this);
-
                 // Setting Dialog Title
+
+                //alertDialog.setView(codeinput);
+                LayoutInflater li = LayoutInflater.from(getBaseContext());
+                View promptsView = li.inflate(R.layout.join_edittext, null);
+
+                // set prompts.xml to alertdialog builder
+                alertDialog.setView(promptsView);
+
+
+                final EditText userInput = (EditText) promptsView
+                        .findViewById(R.id.join_code_edittext);
+
+                exist= false;
                 alertDialog.setTitle("Enter Table Code: ");
                 //Taking COde INput
-                codeinput= new EditText(getApplicationContext());
-                alertDialog.setView(codeinput);
+                //codeinput= new EditText(ResDetails.this);
                 // Setting Positive "Yes" Button
                 alertDialog.setPositiveButton("JOIN", new DialogInterface.OnClickListener() {
+
+
                     public void onClick(DialogInterface dialog,int which) {
-                        if(true) {
+                        reference.addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+                                if(dataSnapshot.hasChild(userInput.getText().toString()) && !exist) {
 
-                            Intent intent = new Intent(getApplicationContext(), Table_Menu.class);
-                            startActivity(intent);
-                            // Write your code here to invoke YES event
-                            Toast.makeText(getApplicationContext(), "Adding you to the Table", Toast.LENGTH_SHORT).show();
+                                    User user = new User();
+                                    user.setName("Ansh123211");
 
-                        }
+                                    reference.child(userInput.getText().toString()).push().setValue(user);
+                                    Intent intent = new Intent(getApplicationContext(), Table_Menu.class);
+                                    startActivity(intent);
+
+                                    // Write your code here to invoke YES event
+                                    Toast.makeText(getApplicationContext(),userInput.getText().toString(), Toast.LENGTH_SHORT).show();
+
+                                    exist= true;
+                                }
+                                else
+                                    Toast.makeText(getApplicationContext(),"Incorrect Code", Toast.LENGTH_SHORT).show();
+
+                            }
+
+                            @Override
+                            public void onCancelled(DatabaseError databaseError) {
+
+                            }
+                        });
                         // Write your code here to invoke YES event
-                        Toast.makeText(getApplicationContext(), "Table Joined", Toast.LENGTH_SHORT).show();
+                       // Toast.makeText(getApplicationContext(), "Table Joined", Toast.LENGTH_SHORT).show();
                     }
                 });
 
